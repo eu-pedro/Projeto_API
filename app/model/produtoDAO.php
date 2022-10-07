e724d13a2207ce97013a1d08f1d84974a03afc47
Bruno Costa <analistabrunocosta@gmail.com>
2022-10-07 20:38:29 -0300

    Finalizado Projeto API

--- app/model/produtoDAO.php
+++ app/model/produtoDAO.php
@@ -19,6 +19,20 @@ class ProdutoDAO{
             throw new \Exception("Nenhum dado encontrado");// Estamos lançando um erro para ser tratado pelo catch
         }
     }
+    public function consultarUnico($id){
+        $comando = "SELECT * FROM {$this->tabela} WHERE cod_produto = :id";
+
+        $preparacao = Conexao::getConexao()->prepare($comando);
+        $preparacao->bindValue(":id",$id);
+        $preparacao->execute();
+
+        if($preparacao->rowCount() > 0){
+            return $preparacao->fetchALL(\PDO::FETCH_ASSOC);
+        }
+        else{
+            throw new \Exception("Nenhum dado encontrado");// Estamos lançando um erro para ser tratado pelo catch
+        }
+    }
     //post
     public function inserir($dados){
         $comando = "INSERT INTO {$this->tabela} VALUES(NULL,:nome, :preco, :info)";
@@ -36,6 +50,47 @@ class ProdutoDAO{
         }
         else{
             throw new \Exception("Erro ao cadastrar as informações");
+        }
+    }
+    //put
+    public function atualizar($id, $dados){
+        $comando = "UPDATE {$this->tabela} SET 
+        nome = :nome_produto,
+        preco = :preco_produto,
+        info_produto = :info WHERE cod_produto = :id";
+
+        $preparacao = Conexao::getConexao()->prepare($comando);
+
+        list($nome, $preco, $info) = array_values($dados);
+        
+        $preparacao->bindValue(':nome_produto', $nome);
+        $preparacao->bindValue(':preco_produto', $preco);
+        $preparacao->bindValue(':info', $info);
+        $preparacao->bindValue(':id', $id);
+
+        $preparacao->execute();
+        if($preparacao->rowCount() > 0){
+            return "Dados atualizados com sucesso";
+        }
+        else{
+            throw new \Exception("Erro ao tentar atualizar os dados");
+        }
+        
+    }
+    //delete
+    public function deletar($id){
+        $comando = "DELETE FROM {$this->tabela} WHERE cod_produto = :id";
+
+        $preparacao = Conexao::getConexao()->prepare($comando);
+        $preparacao->bindValue(":id", $id);
+
+        $preparacao->execute();
+
+        if($preparacao->rowCount() > 0){
+            return "Dados excluídos com sucesso";
+        }
+        else{
+            throw new \Exception("Erro ao tentar excluir os dados");
         }
     }
 }
